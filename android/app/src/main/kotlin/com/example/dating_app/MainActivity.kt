a2158b8fe83fb0c5f340042fd3ae6b934c4b1a47
anyijukirjanett <anyijukirjanett@gmail.com>
2023-12-19 18:54:42 +0300

    4chenges

--- android/app/src/main/kotlin/com/example/dating_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dating_app/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.aramanservices.lite
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+    }
 }
+
