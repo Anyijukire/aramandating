09aa416c830dd35a1a421c8f056b61bbd5dd2861
treva-123mutebi <mdavid.trevor@gmail.com>
2024-08-28 11:01:32 +0300

    Sign in with Google class and edits on sign in method in PhoneNumberScreen class

--- lib/screens/phone_number_screen.dart
+++ lib/screens/phone_number_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:dating_app/dialogs/progress_dialog.dart';
 import 'package:dating_app/helpers/app_localizations.dart';
@@ -10,8 +11,11 @@ import 'package:dating_app/screens/verification_code_screen.dart';
 import 'package:dating_app/widgets/default_button.dart';
 import 'package:dating_app/widgets/show_scaffold_msg.dart';
 import 'package:dating_app/widgets/svg_icon.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shimmer/shimmer.dart';
 
 class PhoneNumberScreen extends StatefulWidget {
   const PhoneNumberScreen({Key? key}) : super(key: key);
@@ -126,6 +130,8 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
         ));
   }
 
+
+
   /// Navigate to next page
   void _nextScreen(screen) {
     // Go to next page route
@@ -135,50 +141,279 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
     });
   }
 
-  /// Sign in with phone number
+  /// Helper function to show ScaffoldMessenger message
+  void showScaffoldMessage({
+    required BuildContext context,
+    required String message,
+    required Color bgcolor,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      backgroundColor: bgcolor,
+    ));
+  }
+  /// Function to check if the user exists by phone number
+  Future<DocumentSnapshot?> _checkIfUserExists(String phoneNumber) async {
+    try {
+      // Reference to the 'Users' collection
+      CollectionReference users =
+          FirebaseFirestore.instance.collection('Users');
+
+      // Query to check if the user exists
+      QuerySnapshot querySnapshot =
+          await users.where('user_phone_number', isEqualTo: phoneNumber).get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return querySnapshot.docs.first;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print("Error checking user existence: $e");
+      return null;
+    }
+  }
+  /// Function to migrate user data from old document to new document
+  Future<void> _handleGoogleSignInAndMigrateUser(
+      DocumentSnapshot oldUserDoc) async {
+    try {
+      // Assuming the user is already signed in with Google and their UID is available
+      User? googleUser = FirebaseAuth.instance.currentUser;
+
+      if (googleUser != null) {
+        // Reference to the 'Users' collection
+        CollectionReference users =
+            FirebaseFirestore.instance.collection('Users');
+
+        // Create a new document with the UID returned by Google
+        DocumentReference newUserDoc = users.doc(googleUser.uid);
+
+        // Copy all fields from the old document to the new one
+        await newUserDoc.set({
+          ...oldUserDoc.data() as Map<String, dynamic>,
+          'isGoogleVerified': true, // Add the boolean value
+        });
+
+        // Delete the old document
+        await oldUserDoc.reference.delete();
+
+        // Show success message
+        showScaffoldMessage(
+            context: context,
+            message: "User data migrated successfully.",
+            bgcolor: Colors.green);
+
+        // Proceed with further authentication or app flow
+        await UserModel().authUserAccount(
+            updateLocationScreen: () =>
+                _nextScreen(const UpdateLocationScreen()),
+            signUpScreen: () => _nextScreen(const SignUpScreen()),
+            homeScreen: () => _nextScreen(const HomeScreen()),
+            blockedScreen: () => _nextScreen(const BlockedAccountScreen()));
+      } else {
+        // Handle the case where the Google user is not found (unlikely if Google Auth was handled correctly)
+        showScaffoldMessage(
+            context: context,
+            message: "Google user not found.",
+            bgcolor: Colors.red);
+      }
+    } catch (e) {
+      print("Error migrating user data: $e");
+      showScaffoldMessage(
+          context: context,
+          message: "An error occurred during data migration.",
+          bgcolor: Colors.red);
+    }
+  }
+
+  /// Function to create a new user document
+  Future<void> _createNewUser(String phoneNumber, BuildContext context) async {
+    try {
+      // Get the current Firebase user
+      User? firebaseUser = FirebaseAuth.instance.currentUser;
+
+      if (firebaseUser != null) {
+        // Reference to the 'Users' collection
+        CollectionReference users =
+            FirebaseFirestore.instance.collection('Users');
+
+        // Create a new document with the UID returned by Firebase Auth
+        DocumentReference newUserDoc = users.doc(firebaseUser.uid);
+
+        // Define the user data
+        Map<String, dynamic> userData = {
+          'user_phone_number': phoneNumber,
+          'isGoogleVerified': true, // Set this to true if Google Auth is done
+          'email': firebaseUser.email, // Get from Firebase user object
+          'name': firebaseUser.displayName, // Get from Firebase user object
+          // Add other fields as necessary
+        };
+
+        // Create the new user document
+        await newUserDoc.set(userData);
+
+        // Show success message
+        showScaffoldMessage(
+            context: context,
+            message: "User account created successfully.",
+            bgcolor: Colors.green);
+
+        // Redirect to the desired screen, e.g., home screen
+        _nextScreen(const  SignUpScreen());
+      } else {
+        // Handle the case where the Firebase user is not found (unlikely after auth)
+        showScaffoldMessage(
+            context: context,
+            message: "Unable to create user account.",
+            bgcolor: Colors.red);
+      }
+    } catch (e) {
+      print("Error creating new user: $e");
+      showScaffoldMessage(
+          context: context,
+          message: "An error occurred while creating the user account.",
+          bgcolor: Colors.red);
+    }
+  }
+
   void _signIn(BuildContext context) async {
     // Show progress dialog
     _pr.show(_i18n.translate("processing"));
 
-    /// Verify user phone number
-    await UserModel().verifyPhoneNumber(
-        phoneNumber: _phoneCode! + _numberController.text.trim(),
-        checkUserAccount: () {
-          /// Authenticate User Account
-          UserModel().authUserAccount(
+    // Get the full phone number and remove leading zero if present
+    String phoneNumber = _numberController.text.trim();
+    if (phoneNumber.startsWith('0')) {
+      phoneNumber = phoneNumber.substring(1); // Remove leading zero
+    }
+    phoneNumber = _phoneCode! + phoneNumber;
+    debugPrint(phoneNumber);
+
+    // Perform the search for the phone number
+    DocumentSnapshot? userDoc = await _checkIfUserExists(phoneNumber);
+
+    // Hide the progress dialog
+    _pr.hide();
+
+    if (userDoc != null) {
+      // Show success message if the user exists
+      showScaffoldMessage(
+          context: context,
+          message: "User with phone number $phoneNumber found.",
+          bgcolor: Colors.green);
+
+      // Check if the user is already Google verified or if the field is missing
+      if (userDoc.data() != null &&
+          (userDoc.data() as Map<String, dynamic>)
+              .containsKey('isGoogleVerified')) {
+        bool isGoogleVerified = userDoc.get('isGoogleVerified');
+
+        if (isGoogleVerified) {
+          // User is already Google verified, proceed to authentication
+          await UserModel().authUserAccount(
               updateLocationScreen: () =>
                   _nextScreen(const UpdateLocationScreen()),
               signUpScreen: () => _nextScreen(const SignUpScreen()),
               homeScreen: () => _nextScreen(const HomeScreen()),
               blockedScreen: () => _nextScreen(const BlockedAccountScreen()));
-          // END
-        },
-        codeSent: (code) async {
-          // Hide progreess dialog
-          _pr.hide();
-          // Go to verification code screen
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => VerificationCodeScreen(
-                    verificationId: code,
-                  )));
-        },
-        onError: (errorType, msg) async {
-          // Hide progreess dialog
-          _pr.hide();
+        } else {
+          // User is not yet Google verified, proceed with migration
+          await _handleGoogleSignInAndMigrateUser(userDoc);
+        }
+      } else {
+        // Field does not exist, proceed with migration
+        await _handleGoogleSignInAndMigrateUser(userDoc);
+      }
+    } else {
+      // Show error message if the user does not exist
+      await _createNewUser(phoneNumber, context);
+    }
+  }
 
-          // Check Erro type
-          if (errorType == 'invalid_number') {
-            // Check error type
-            final String message =
-                _i18n.translate("we_were_unable_to_verify_your_number") +
-                    "\nError: $msg";
-            // Show error message
-            // Validate context
-            if (mounted) {
-              showScaffoldMessage(
-                  context: context, message: message, bgcolor: Colors.red);
-            }
-          }
-        });
+}
+
+class GoogleSignin extends StatefulWidget {
+  @override
+  _GoogleSigninState createState() => _GoogleSigninState();
+}
+
+class _GoogleSigninState extends State<GoogleSignin> {
+   bool _isLoading = false;
+
+  Future<User?> _signInWithGoogle({required BuildContext context}) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+            await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => PhoneNumberScreen()));
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          // handle the error here
+        } else if (e.code == 'invalid-credential') {
+          // handle the error here
+        }
+      } catch (e) {
+        // handle the error here
+      }
+    }
+
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Disable back navigation
+      ),
+      body: _isLoading
+          ? Center(child: Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Container(
+                width: 200,
+                height: 20,
+                color: Colors.white,
+              ),
+            ))
+          : ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Signing you in...'),
+                  ),
+                );
+                _signInWithGoogle(context: context);
+              },
+              child: Text('Continue with Google'),
+            ),
+    );
   }
 }
+
+
+
+
